5916cf170bf486646bb6bb204e079b40e2175394
Maimun54 <maimunislam@gmail.com>
2023-11-09 18:08:21 +0600

    fixed vercel issue

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const express = require('express')
 const app =express();
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000
 
 //middleware 
 app.use(cors())
 app.use(express.json());
-
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pl4mu3l.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,12 +26,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
    
     const allBlogCollection =client.db('ChildCareDB').collection('allBlog');
     const wishListBlogCollection =client.db('ChildCareDB').collection('wishlist');
     const newLetterCollection =client.db('ChildCareDB').collection('newLetter');
+    const awarenessCollection =client.db('ChildCareDB').collection('recentBlog');
 
     
     app.post('/subscribe',async(req,res)=>{
@@ -55,18 +54,25 @@ async function run() {
       const cursor =allBlogCollection.find(queryObj)
       const result =await cursor.toArray()
       res.send(result)
-    })
+    }) 
      app.get('/allBlog/:id',async(req,res)=>{
       const id =req.params.id;
       const query ={_id:new ObjectId(id)}
       const result =await allBlogCollection.findOne(query)
-      
+       
+
       res.send(result)
     })
      app.get('/wishlist/:id',async(req,res)=>{
       const id =req.params.id;
       const query ={_id:new ObjectId(id)}
       const result =await wishListBlogCollection.findOne(query)
+      
+      res.send(result)
+    })
+     app.get('/recentBlog',async(req,res)=>{
+      const query =req.body
+      const result =await awarenessCollection.find(query).toArray()
       
       res.send(result)
     })
@@ -94,11 +100,9 @@ async function run() {
    //
    app.get('/wishlist',async(req,res)=>{
     console.log(req.query.email)
-    // const query=req.body
+    
     let query ={};
-    // if(req.query.email !==req.user?.email){
-    //   return res.status(403).send({message:'forbidden access'})
-    // }
+   
     if(req.query?.email){
       query ={email: req.query.email}
     }
@@ -129,7 +133,7 @@ async function run() {
    res.send(result)
   })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
